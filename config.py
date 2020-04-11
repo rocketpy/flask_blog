13418c9b7c3219b015599a7ac876fa625fe9da5c
rocketpy <dimitr.ushakov@gmail.com>
2020-04-12 00:00:47 +0300

    modified

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+SECRET_KEY = 'SECRET_KEY'
+WTF_CSRF_SECRET_KEY = "CSRF_SECRET_KEY"
+CSRF_ENABLED = True
 
+# app.config['SECRET_KEY'] = 'SECRET_KEY'
+# app.config['WTF_CSRF_SECRET_KEY'] = "CSRF_SECRET_KEY"
+# app.config['CSRF_ENABLED'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+# SECRET_KEY = os.urandom(32)
